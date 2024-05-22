7133a015d372e68db563ccf83526f9ed96aa4bec
Сергей <lockup86@rambler.ru>
2024-05-22 22:00:53 +0300

    Hm11.6

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,22 @@ import java.time.LocalDate;
 
 public class Main {
     // Задача №1
-    public static void leapYear (int a){
-        if (a % 4 ==0 && a % 100 != 0 || a % 400 == 0 )
-            System.out.println(a+ " год — високосный год");
+    public static void leapYear(int a) {
+        if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0)
+            System.out.println(a + " год — високосный год");
         else {
-            System.out.println(a+ " год — невисокосный год");
+            System.out.println(a + " год — невисокосный год");
         }
     }
 
     // Задача №2
     public static void operatingSystem(int a, int b) {
         int currentYear = LocalDate.now().getYear();
-        if ( b < 0 || b > 1) {
-            System.out.println("Такой операционной системы нет");
-        }else {
+        if (b == 0 || b == 1) {
             if (a < currentYear && b == 1) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             }
-            if (a == currentYear && b == 1) {// я поменял а>=currentYear т.к currentYear это текущее время . Получается если оно будет больше то это операционная система из будущего и ей нет смысла устанавливать прошлую версию )
+            if (a == currentYear && b == 1) {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
             if (a < currentYear && b == 0) {
@@ -28,27 +26,30 @@ public class Main {
             if (a == currentYear && b == 0) {
                 System.out.println("Установите версию приложения для iOS по ссылке");
             }
+
+        } else {
+            System.out.println("Такой операционной системы нет");
         }
     }
 
     // Задача №3
     public static int deliveryDuration(int a) { // можно было бы и void  поставить
-        if (a  > 100) {
-            return -1 ;
+        if (a > 100) {
+            return -1;
         }
-        int day = 1;
-        if (a >= 20 ) {
-            day++;
+        int day = 0;
+        if (a >= 0) {
+            day += 1;
+        }
+        if (a >= 20) {
+            day += 1;
         }
         if (a >= 60) {
-            day++;
+            day += 1;
+
         }
         return day;
-
-
-
     }
-
 
 
     public static void main(String[] args) {
@@ -59,16 +60,16 @@ public class Main {
         System.out.println();
 
         // Задача №2
-        int clientOS = 1;
+        int clientOS = 2;
         int clientDeviceYear = 2023;
-        operatingSystem(clientDeviceYear , clientOS);
+        operatingSystem(clientDeviceYear, clientOS);
 
 
         System.out.println();
 
         // Задача №3
 
-        int distans = deliveryDuration(20);
+        int distans = deliveryDuration(19);
         var deliviry = distans > 0 ? "Потребуется дней: " + distans : "Доставки нет";
         System.out.println(deliviry);
 
